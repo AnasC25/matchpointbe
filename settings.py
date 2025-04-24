bf41ac08ba5d0fe1df70c45d95127b01579468c6
AnasC25 <charradouanas@gmail.com>
2025-04-24 13:26:12 +0100

    Whitelist verce

--- settings.py
+++ settings.py
@@ -27,12 +27,14 @@ CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://matchpointfront.vercel.app",
     "https://bookish-space-carnival-v46qwgxr799fp64g-8000.app.github.dev",
     "http://ec2-44-201-157-94.compute-1.amazonaws.com"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
+    "https://matchpointfront.vercel.app",
     "https://bookish-space-carnival-v46qwgxr799fp64g-8000.app.github.dev",
     "http://ec2-44-201-157-94.compute-1.amazonaws.com"
 ]
